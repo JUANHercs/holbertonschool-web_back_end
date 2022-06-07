3f7eda088c9190bdeaef088cb9d8bd8fce5c85c3
JUANHercs <juan.hercs@gmail.com>
2022-06-07 04:10:30 +0000

    fix_spaces_in_functon_parameters

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,7 +3,7 @@
 b as arguments and returns their sum as a float."""
 
 
-def add(a:float, b:float) -> float:
+def add(a: float, b: float) -> float:
     """function that sum two floats and return a float with annotations"""
     return a + b
 
