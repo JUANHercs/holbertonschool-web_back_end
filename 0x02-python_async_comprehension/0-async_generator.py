7b216925b1eb8a0c7efe278d5da082ead98f1178
JUANHercs <juan.hercs@gmail.com>
2022-06-08 19:15:16 +0000

    info_first

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Async Generator"""
+import asyncio
+import random
+
+async def async_generator():
+    delay = delay.sleep(1)
+    await async_generatot.delay
+    yield random.uniform(0, 10)
