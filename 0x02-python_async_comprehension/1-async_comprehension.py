7431f7ce0e11e5ef381b17994c23884d6e8f6b7e
JUANHercs <juan.hercs@gmail.com>
2022-06-08 23:47:14 -0500

    requiriment

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-""" Async comprehensing"""
+""" Async comprehensing """
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """
-    use an async comprehension over async_generator, 
-    returning 10 random numbers
-    """
+    """use an async comprehension over async_generator, 
+    returning 10 random numbers"""
     return [i async for i in async_generator()]
